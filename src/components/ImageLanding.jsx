c370b93b7f2f94303ac8de658327b407096f1704
Tsaniah Munfidah <www.tsaniahmunfidah@gmail.com>
2021-11-05 11:33:46 +0700

    done

--- src/components/ImageLanding.jsx
+++ src/components/ImageLanding.jsx
@@ -1,5 +1,5 @@
 const ImageLanding = () => {
-    return <div className="container col-xxl-8 px-4 py-sm">
+    return <div className="container col-xxl-8 px-4 py-sm min-vh-100">
     <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
       <div className="col-10 col-sm-8 col-lg-6">
         <img
