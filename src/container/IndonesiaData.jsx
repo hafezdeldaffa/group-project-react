56bd93d490c429e83c8f9bde78a104993a145a74
Hafezd El Daffa <hafezdeldaffa9@gmail.com>
2021-11-07 21:49:02 +0700

    create: routes in navbar

---
+++ src/container/IndonesiaData.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "../components/Navbar"
+
+const IndonesiaData = () => {
+    return (
+        <React.Fragment>
+            <Navbar/>
+        </React.Fragment>
+    )
+}
+
+export default IndonesiaData
