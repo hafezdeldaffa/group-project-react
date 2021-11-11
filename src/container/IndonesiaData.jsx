868977e5f19443e35d503866fc11567f3bb09537
Tsaniah Munfidah <www.tsaniahmunfidah@gmail.com>
2021-11-11 19:09:40 +0700

    create table and chart

--- src/container/IndonesiaData.jsx
+++ src/container/IndonesiaData.jsx
@@ -2,16 +2,32 @@ import React, { useContext } from 'react';
 import CardCase from '../components/CardCase';
 import Navbar from '../components/Navbar';
 import { IndonesiaContext } from '../context/IndonesiaContext';
+import ProvinceDataTable from '../components/ProvinceDataTable';
+import { ProvinceContext } from '../context/ProvinceContext';
+import ChartIndo from '../components/ChartIndo';
+import SortAndSearch from '../components/SortAndSearch';
+import { IndonesiaDailyContext } from '../context/IndonesiaDailyContext';
+import { IndonesiaDailyContextProvider } from '../context/IndonesiaDailyContext';
 
 const IndonesiaData = () => {
+
   const [cases] = useContext(IndonesiaContext);
+  const [ProvCases] = useContext(ProvinceContext);
+  
 
   return (
     <React.Fragment>
-      <Navbar />
-      {cases.map((cases) => {
-        return <CardCase key={cases.length} cases={cases} />;
-      })}
+      <div className="bg-light">
+        <Navbar />
+        {cases.map((cases) => {
+          return <CardCase key={cases.length} cases={cases} />;
+        })}
+        <IndonesiaDailyContextProvider>
+        <ChartIndo/>
+        </IndonesiaDailyContextProvider>
+        <SortAndSearch />
+        <ProvinceDataTable key={ProvCases.length} ProvCases={ProvCases} />
+      </div>
     </React.Fragment>
   );
 };
