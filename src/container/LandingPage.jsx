359bb6b6937b4e82f4e4a3090c91d02054c11713
chandrapurba <chandrapurba24@gmail.com>
2021-11-14 22:32:08 +0700

    update LandingPage

--- src/container/LandingPage.jsx
+++ src/container/LandingPage.jsx
@@ -3,6 +3,10 @@ import ImageLanding from '../components/ImageLanding';
 import Navbar from '../components/Navbar';
 import ServiveInfo from '../components/ServiceInfo';
 import SymptomSection from '../components/SymptomSection';
+import IndonesiaCases from '../components/IndonesiaCases';
+import BusinessProcess from '../components/BusinessProcess';
+import Fitur from '../components/Fitur';
+import Footer from '../components/Footer';
 
 
 const LandingPage = () => {
@@ -12,6 +16,10 @@ const LandingPage = () => {
             <ImageLanding/>
             <ServiveInfo/>
             <SymptomSection/>
+            <IndonesiaCases/>
+            <BusinessProcess/>
+            <Fitur/>
+            <Footer/>
         </React.Fragment>
     )
 }
