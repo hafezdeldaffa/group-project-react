d1171729f2c9a291735b49bb8555e5559e357afc
Tsaniah Munfidah <www.tsaniahmunfidah@gmail.com>
2021-11-15 00:31:02 +0700

    updte Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,69 +5,67 @@ const Search = ({ ...data }) => {
     const found = data.ProvCases.find(({ provinsi }) => {
         const result = provinsi.toLowerCase().indexOf(search.toLowerCase());
 
-        if(result >=0){
+        if (result >= 0) {
             return true
-        }else{
+        } else {
             return false
         }
     })
     return (
         <>
             <div className="container  my-3">
-                <div className="search-box">
-                    <a className="search-btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                        <img className="btn-icon" src="/images/search.png" />
-                    </a>
-                    <input className="search-input mx-3" type="text" name="" placeholder="Pencarian" minlength={4} onChange={(c) => setSearch(c.target.value)} />
+                <div className="search-box shadow">
+                    <img className="btn-icon search-btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop" src="/images/search.png" alt="search" />
+                    <input className="search-input mx-3" type="text" name="" placeholder="Pencarian" onChange={(c) => setSearch(c.target.value)} />
                 </div>
             </div>
-            
-            <div class="modal fade " id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title text-coral" id="staticBackdropLabel">Hasil Pencarian</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+            <div className="modal fade " id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title text-coral" id="staticBackdropLabel">Hasil Pencarian</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
+                        <div className="modal-body">
                             {
                                 found === undefined ? <h1>Not Found</h1> :
                                     <>
                                         <h1 className="text-center text-capitalize mb-4">Provinsi {found.provinsi.toLowerCase()}</h1>
 
-                                        <div id="card" class="row g-2">
-                                        <div class="col-sm-6 col-12">
-                                            <div class="card w-100 border-primary bg-search-card bg-gradient">
-                                                <div class="card-body">
-                                                    <h5 class="card-title text-capitalize text-white">Positif</h5>
-                                                    <h2 class="card-text fw-bold text-white">{found.kasus}</h2>
+                                        <div id="card" className="row g-2">
+                                            <div className="col-sm-6 col-12">
+                                                <div className="card w-100 border-primary bg-search-card bg-gradient">
+                                                    <div className="card-body">
+                                                        <h5 className="card-title text-capitalize text-white">Positif</h5>
+                                                        <h2 className="card-text fw-bold text-white">{found.kasus}</h2>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="col-sm-6 col-12">
-                                            <div class="card w-100 border-primary bg-search-card bg-gradient">
-                                                <div class="card-body">
-                                                    <h5 class="card-title text-capitalize text-white">Sembuh</h5>
-                                                    <h2 class="card-text fw-bold text-white">{found.sembuh}</h2>
+                                            <div className="col-sm-6 col-12">
+                                                <div className="card w-100 border-primary bg-search-card bg-gradient">
+                                                    <div className="card-body">
+                                                        <h5 className="card-title text-capitalize text-white">Sembuh</h5>
+                                                        <h2 className="card-text fw-bold text-white">{found.sembuh}</h2>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="col-sm-6 col-12">
-                                            <div class="card w-100 border-primary bg-search-card bg-gradient">
-                                                <div class="card-body">
-                                                    <h5 class="card-title text-capitalize text-white">Meninggal</h5>
-                                                    <h2 class="card-text fw-bold text-white">{found.meninggal}</h2>
+                                            <div className="col-sm-6 col-12">
+                                                <div className="card w-100 border-primary bg-search-card bg-gradient">
+                                                    <div className="card-body">
+                                                        <h5 className="card-title text-capitalize text-white">Meninggal</h5>
+                                                        <h2 className="card-text fw-bold text-white">{found.meninggal}</h2>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="col-sm-6 col-12">
-                                            <div class="card w-100 border-primary bg-search-card bg-gradient">
-                                                <div class="card-body">
-                                                    <h5 class="card-title text-capitalize text-white">Dirawat</h5>
-                                                    <h2 class="card-text fw-bold text-white">{found.dirawat}</h2>
+                                            <div className="col-sm-6 col-12">
+                                                <div className="card w-100 border-primary bg-search-card bg-gradient">
+                                                    <div className="card-body">
+                                                        <h5 className="card-title text-capitalize text-white">Dirawat</h5>
+                                                        <h2 className="card-text fw-bold text-white">{found.dirawat}</h2>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
                                         </div>
                                     </>
                             }
