1a0db8e2939b02cdc7ff615bbea34320c04486f1
Tsaniah Munfidah <www.tsaniahmunfidah@gmail.com>
2021-11-15 11:37:04 +0700

    Update Fitur

--- src/components/Fitur.jsx
+++ src/components/Fitur.jsx
@@ -1,27 +1,23 @@
 const Fitur = () => {
     return (
         <div className="container">
-            <div className="row">
+            <div className="row d-flex align-items-center ">
                 <div className="col-12 col-lg-6">
-                    <div className="mt-5"><h4 className= "fw-bold"> Fitur Dari Neimo </h4>
-                    <p> Neimo memberikan kemudahan bagi kepala RT serta masyarakat dalam 
-                        pelacakan COVID-19 di lingkungan sekitar. Fitur yang ditawarkan Neimo meliputi :  
-                    </p>
-                    <ul>
-  <li>Laporan Kasus Positif dan Vaksinasi</li>
-  <li>Visualisasi Data Statistika Covid-19</li>
-  <li>Cari Rumah Sakit dan Ambulans</li>
-</ul> 
+                    <div className=""><h4 className="fw-bold"> Fitur Dari Neimo </h4>
+                        <p> Neimo memberikan kemudahan bagi kepala RT serta masyarakat dalam
+                            pelacakan COVID-19 di lingkungan sekitar. Fitur yang ditawarkan Neimo meliputi :
+                        </p>
+                        <ul>
+                            <li>Laporan Kasus Positif dan Vaksinasi</li>
+                            <li>Visualisasi Data Statistika Covid-19</li>
+                            <li>Cari Rumah Sakit dan Ambulans</li>
+                        </ul>
                     </div>
-
-
-                    
-                        
                 </div>
                 <div className="col-12 col-lg-6">
-                
+
                     <div className="p-5">
-                    <img src="/images/undraw_Dropdown_menu_re_jqqs.png" width = "100%"/> 
+                        <img src="/images/undraw_Dropdown_menu_re_jqqs.png" width="100%" />
                     </div>
                 </div>
             </div>
