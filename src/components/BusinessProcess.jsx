15747ebad7bacb731680cc0ece346a84d9d2b653
Tsaniah Munfidah <www.tsaniahmunfidah@gmail.com>
2021-11-15 11:38:38 +0700

    update Business Process

--- src/components/BusinessProcess.jsx
+++ src/components/BusinessProcess.jsx
@@ -1,21 +1,18 @@
 const BusinessProcess = () => {
     return (
         <div className="container">
-            <div className="row">
+            <div className="row d-flex align-items-center">
                 <div className="col-12 col-lg-6">
-                    <div className="mt-5"><h4 className= "fw-bold"> Bagaimana Neimo Bekerja? </h4>
+                    <div className=" mt-5">
+                    <h4 className= "fw-bold"> Bagaimana Neimo Bekerja? </h4>
                     <p> Aplikasi ini mengandalkan partisipasi masyarakat untuk 
                         saling membagikan data terkait COVID-19, seperti
                         vaksinasi serta kesehatan dari keluarga mereka 
                     </p>
-                    <button type="button" class="btn btn-outline-dark px-5">Pelajari</button></div>
-
-
-                    
-                        
+                    <button type="button" class="btn btn-outline-dark px-5">Pelajari</button>
+                    </div> 
                 </div>
                 <div className="col-12 col-lg-6">
-                
                     <div className="p-5">
                     <img src="/images/undraw_Usability_testing_re_uu1g.png" width = "100%"/> 
                     </div>
